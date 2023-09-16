3d0b4a54fa30c07b30194e35cf8ea0e75d4f4c09
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-09-16 14:40:12 -0500

    populated main method

--- assignment9/src/main/java/com/wonderwebdev/assignment9/Assignment9Application.java
+++ assignment9/src/main/java/com/wonderwebdev/assignment9/Assignment9Application.java
@@ -1,13 +1,35 @@
 package com.wonderwebdev.assignment9;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.wonderwebdev.assignment9")
 public class Assignment9Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Assignment9Application.class, args);
+	
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(assignment9Application.class, args);
+		
+		// example of how to parse a CSV file 
+		Reader in = new FileReader("recipes.txt");
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT
+											   .withIgnoreSurroundingSpaces()
+											   .withHeader()
+											   .withSkipHeaderRecord()
+											   .parse(in);
+		for (CSVRecord record : records) {
+			int cookingMinutes = Integer.parseInt(record.get("Cooking Minutes"));
+			boolean dairyFree = Boolean.parseBoolean(record.get("Dairy Free"));
+		}
 	}
+
 
 }
