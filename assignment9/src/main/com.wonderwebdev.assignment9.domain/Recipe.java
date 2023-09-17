520ef750a50482589ce5ca515d5fbb69cac855b7
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-09-16 19:44:00 -0500

    Populated the Recipe.java POJO

--- assignment9/src/main/com.wonderwebdev.assignment9.domain/Recipe.java
+++ assignment9/src/main/com.wonderwebdev.assignment9.domain/Recipe.java
@@ -1,3 +1,119 @@
-public class recipe {
+package com.wonderwebdev.assignment9.domain;
+
+public class Recipe {
+  private Integer cookingMinutes;
+    private Boolean dairyFree;
+    private Boolean glutenFree;
+    private String instructions;
+     private Double preparationMinutes;
+    private Double pricePerServing;
+    private Double readyInMinutes;
+    private Integer servings;
+    private Double spoonacularScore;
+    private String title;
+    private Boolean vegan;
+    private Boolean vegetarian;
+    
+    public Recipe() {
+        
+    }
+    //insert getters and setters here
+
+    public Integer getCookingMinutes() {
+        return cookingMinutes;
+    }
+
+    public void setCookingMinutes(Integer cookingMinutes) {
+        this.cookingMinutes = cookingMinutes;
+    }
+
+    public Boolean getDairyFree() {
+        return dairyFree;
+    }
+
+    public void setDairyFree(Boolean dairyFree) {
+        this.dairyFree = dairyFree;
+    }
+
+
+    public Boolean getGlutenFree() {
+        return glutenFree;
+    }
+
+    public void setGlutenFree(Boolean glutenFree) {
+        this.glutenFree = glutenFree;
+    }
+
+    public Double getPreparationMinutes() {
+        return preparationMinutes;
+    }
+
+    public void setPreparationMinutes(Double preparationMinutes) {
+        this.preparationMinutes = preparationMinutes;
+    }
+
+    public Double getPricePerServing() {
+        return pricePerServing;
+    }
+
+    public void setPricePerServing(Double pricePerServing) {
+        this.pricePerServing = pricePerServing;
+    }
+
+    public Double getReadyInMinutes() {
+        return readyInMinutes;
+    }
+
+    public void setReadyInMinutes(Double readyInMinutes) {
+        this.readyInMinutes = readyInMinutes;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public Double getSpoonacularScore() {
+        return spoonacularScore;
+    }
+
+    public void setSpoonacularScore(Double spoonacularScore) {
+        this.spoonacularScore = spoonacularScore;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+      public Boolean getVegan() {
+        return vegan;
+    }
+
+    public void setVegan(Boolean vegan) {
+        this.vegan = vegan;
+    }
+      public Boolean getVegetarian() {
+        return vegetarian;
+    }
+
+
+    public void setVegetarian(Boolean vegetarian) {
+        this.vegetarian = vegetarian;
+    }
+    public String getInstructions() {
+        return instructions;
+    }
+
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
     
 }
